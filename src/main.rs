3421abfdd8e8b453a84f4ed481546aaf16559eff
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-05-20 23:52:50 -0400

    display full price values

--- src/main.rs
+++ src/main.rs
@@ -171,7 +171,7 @@ fn asset2article(asset: &Token) -> InlineQueryResult {
         .and_then(|pt| pt.value)
         .map_or_else(
             || "Price token or value is missing".to_string(),
-            |value| format!("{} = {:.3} USD", asset.symbol, value),
+            |value| format!("{} = {} USD", asset.symbol, value),
         );
 
     // let price_24hr_change = asset.price_24hr_change.parse::<f64>().unwrap_or_default();
